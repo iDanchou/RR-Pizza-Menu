32ae6b00b63d56ad21af0a0c6aed7c2ae89b96b3
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-04-16 16:08:55 -0700

    Completed project, brushed up on React components and syntax

--- src/Components/menu.js
+++ src/Components/menu.js
@@ -3,14 +3,29 @@ import pizzaData from "./data";
 import "./index.css";
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+
+      {numPizzas > 0 ? (
+        <>
+          {/* Fragment shorthand  to allow the use of multiple objects to be rendered*/}
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
     </main>
   );
 }
--- src/Components/pizza.js
+++ src/Components/pizza.js
@@ -1,13 +1,39 @@
 import "./index.css";
 
-function Pizza(props) {
+// function Pizza(props) {
+//   return (
+//     <li className="pizza">
+//       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+//       <div>
+//         <h3>{props.pizzaObj.name}</h3>
+//         <p>{props.pizzaObj.ingredients}</p>
+//         <span>{props.pizzaObj.price}</span>
+//       </div>
+//     </li>
+//   );
+// }
+
+// Destructuring the props object
+
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+
+  // if (pizzaObj.soldOut) return null;
+
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>(pizzaObj.price)</span>
+        )} */}
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
